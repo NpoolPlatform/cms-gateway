9d0789f9328bf2fcc89299afd9e13d85f0ea3919
jakys <jakysing@163.com>
2024-01-10 09:13:41 +0000

    Fix reap head

--- api/article/query.go
+++ api/article/query.go
@@ -79,7 +79,7 @@ func Content(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "%v", err.Error())
 		return
 	}
-	w.Header().Set("Content-Type", "text/html")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprintf(w, "%v", info)
 }
 
