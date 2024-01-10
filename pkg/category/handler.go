78cbfee72a3d8231d4bea097b43ad0a4eb23d6bc
jakys <jakysing@163.com>
2024-01-10 08:16:56 +0000

    Fix parentid req

--- pkg/category/handler.go
+++ pkg/category/handler.go
@@ -110,6 +110,10 @@ func WithParentID(id *string, must bool) func(context.Context, *Handler) error {
 		if err != nil {
 			return err
 		}
+		nilUUID := uuid.Nil.String()
+		if *id == nilUUID {
+			return nil
+		}
 		h.ParentID = id
 		return nil
 	}
